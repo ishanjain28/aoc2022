709eb394cafccaf6f43b6e2d4c07a1c94ce0ef68
Ishan Jain <contact@ishanjain.me>
2022-12-12 00:59:05 +0530

    Refactoring, Added day2

---
+++ src/day2/1.rs
@@ -0,0 +1,65 @@
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn parse(input: &'static str) -> Vec<(Move, Move)> {
+    input
+        .trim()
+        .lines()
+        .map(|set| {
+            let set = set.trim();
+            let (a, b) = set.split_at(1);
+
+            let x = match a.trim() {
+                "A" => Move::Rock,
+                "B" => Move::Paper,
+                "C" => Move::Scissors,
+                _ => unreachable!(),
+            };
+            let y = match b.trim() {
+                "X" => Move::Rock,
+                "Y" => Move::Paper,
+                "Z" => Move::Scissors,
+                v => {
+                    println!("{:?}", v);
+
+                    unreachable!();
+                }
+            };
+            (x, y)
+        })
+        .collect()
+}
+
+#[derive(Clone, Copy)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+fn calc_score(m1: Move, m2: Move) -> i32 {
+    match (m1, m2) {
+        (Move::Rock, Move::Rock) => 3,
+        (Move::Rock, Move::Paper) => 0,
+        (Move::Rock, Move::Scissors) => 6,
+        (Move::Paper, Move::Rock) => 6,
+        (Move::Paper, Move::Paper) => 3,
+        (Move::Paper, Move::Scissors) => 0,
+        (Move::Scissors, Move::Rock) => 0,
+        (Move::Scissors, Move::Paper) => 6,
+        (Move::Scissors, Move::Scissors) => 3,
+    }
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        let output = parse(input);
+        let mut score = 0;
+
+        for (a, b) in output {
+            score += b as i32;
+            score += calc_score(b, a);
+        }
+
+        println!("{:?}", score);
+    }
+}
---
+++ src/day2/2.rs
@@ -0,0 +1,80 @@
+#![feature(test)]
+extern crate test;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn parse(input: &'static str) -> impl Iterator<Item = (Move, Outcome)> {
+    input.trim().lines().map(|set| {
+        let mut set = set.bytes();
+        let (a, b) = (set.next().unwrap(), set.nth(1).unwrap());
+
+        let x = match a {
+            b'A' => Move::Rock,
+            b'B' => Move::Paper,
+            b'C' => Move::Scissors,
+            _ => unreachable!(),
+        };
+        let y = match b {
+            b'X' => Outcome::X,
+            b'Y' => Outcome::Y,
+            b'Z' => Outcome::Z,
+            _ => unreachable!(),
+        };
+        (x, y)
+    })
+}
+
+#[derive(Clone, Copy)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+#[derive(Clone, Copy)]
+enum Outcome {
+    X = 0,
+    Y = 3,
+    Z = 6,
+}
+
+#[inline]
+const fn calc_move(d1: Outcome, m1: Move) -> Move {
+    match (d1, m1) {
+        (Outcome::X, Move::Rock) => Move::Scissors,
+        (Outcome::X, Move::Paper) => Move::Rock,
+        (Outcome::X, Move::Scissors) => Move::Paper,
+        (Outcome::Y, v) => v,
+        (Outcome::Z, Move::Rock) => Move::Paper,
+        (Outcome::Z, Move::Paper) => Move::Scissors,
+        (Outcome::Z, Move::Scissors) => Move::Rock,
+    }
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        let output = parse(input);
+        let score = solution(output);
+
+        println!("{:?}", score);
+    }
+}
+
+fn solution(input: impl Iterator<Item = (Move, Outcome)>) -> i32 {
+    let mut score = 0;
+
+    for (a, b) in input {
+        score += b as i32;
+        score += calc_move(b, a) as i32;
+    }
+    score
+}
+
+#[bench]
+fn solution_bench(b: &mut test::Bencher) {
+    b.iter(|| {
+        let input = parse(INPUTS[1]);
+        let result = solution(input);
+        test::black_box(result);
+    })
+}
