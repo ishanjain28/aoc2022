47e480311dba839a97541505f058b2b6172e60fc
Ishan Jain <contact@ishanjain.me>
2022-12-12 00:55:35 +0530

    Refactoring, Added day1

---
+++ src/day1/1.rs
@@ -0,0 +1,31 @@
+#![feature(test)]
+extern crate test;
+
+const INPUTS: [&'static str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn parse(input: &'static str) -> Vec<Vec<i64>> {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|set| {
+            set.split('\n')
+                .map(|c| c.trim())
+                .map(|c| c.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        let output = parse(input);
+
+        let max = output
+            .into_iter()
+            .map(|c| c.into_iter().sum::<i64>())
+            .max()
+            .unwrap();
+
+        println!("{}", max);
+    }
+}
---
+++ src/day1/2.rs
@@ -0,0 +1,31 @@
+use std::cmp::Reverse;
+
+const INPUTS: [&str; 2] = [include_str!("./sample.txt"), include_str!("./input.txt")];
+
+fn parse(input: &'static str) -> Vec<Vec<i64>> {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|set| {
+            set.split('\n')
+                .map(|c| c.trim())
+                .map(|c| c.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn main() {
+    for input in INPUTS.iter() {
+        let output = parse(input);
+
+        let mut sets: Vec<i64> = output
+            .into_iter()
+            .map(|c| c.into_iter().sum::<i64>())
+            .collect();
+
+        sets.select_nth_unstable_by_key(2, |c| Reverse(*c));
+
+        println!("{}", sets.into_iter().take(3).sum::<i64>());
+    }
+}
